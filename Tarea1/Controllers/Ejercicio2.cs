e882eaddc010143bf7cc5a610b835bb86a8c434e
kaneki-04 <kvin121204@gmail.com>
2025-08-30 19:17:42 -0600

    Primer commit

---
+++ Tarea1/Controllers/Ejercicio2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tarea1.Controllers
+{
+	public class Ejercicio2 : Controller
+	{
+		private List<string> nombres = new List<string>
+		{
+			"Ana", "Luis", "Carlos", "Marta", "Sofía", "Jorge", "Lucía", "Pedro", "Elena", "Diego"
+		};
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public string BuscarPosicion(int posicion)
+		{
+			if (posicion < 0 || posicion >= nombres.Count)
+			{
+				return "Posición inválida";
+			}
+
+			return nombres[posicion];
+		}
+
+		public int BuscarCadena(string nombre)
+		{
+			return nombres.FindIndex(n => n.Equals(nombre));
+		}
+	}
+}
